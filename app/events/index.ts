875c710869ea1b17c4bcc51f9d6c38e9760b5de0
scott james <scottejames@gmail.com>
2017-04-21 13:32:46 +0100

    Moved imports into a barrel to simplify things

---
+++ app/events/index.ts
@@ -0,0 +1,6 @@
+export * from './event-details/index'
+export * from './shared/index'
+export * from './create-event.component'
+export * from './event-thumbnail.component'
+export * from './events-list.component'
+export * from './events-list-resolver.service'
