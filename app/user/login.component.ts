d20ff194e0993e8230a7ce44fed23a4d641b306c
scott james <scottejames@gmail.com>
2017-04-21 18:09:10 +0100

    adding basic validation to login form

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -2,13 +2,17 @@ import {Component} from '@angular/core'
 import {AuthService} from './auth.service'
 
 @Component({
-    templateUrl: 'app/user/login.component.html'
+    templateUrl: 'app/user/login.component.html',
+    styles: [`
+        em {float:right; color:#E05C65; padding-left:10px;}           
+        `]
 })
-export class LoginComponent{
-    constructor (private authService:AuthService){
+export class LoginComponent {
+    constructor(private authService: AuthService) {
 
     }
-    login(formValues){
+
+    login(formValues) {
         this.authService.loginUser(formValues.userName, formValues.password)
         console.log(formValues)
     }
