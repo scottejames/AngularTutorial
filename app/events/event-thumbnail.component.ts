e543d8fde7d8bdfed9ae8c5acd344854b52e5b0f
scott james <scottejames@gmail.com>
2017-04-21 14:17:02 +0100

    Adding types to clean things up

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,5 +1,5 @@
 import {Component,Input,Output,EventEmitter} from '@angular/core'
-
+import {IEvent} from './shared/index'
 @Component ({
 selector : 'event-thumbnail',
 template : `
@@ -24,6 +24,6 @@ styles: [`
 })
 
 export class EventThumbnailComponent {
-  @Input() event : any
+  @Input() event : IEvent
 
 }
