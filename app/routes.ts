f9320f4257c8033527b43c3e7b2812f25f88157e
scott james <scottejames@gmail.com>
2017-04-21 13:21:44 +0100

    Adding user profile in a seperate module, added some stubs to enable more detailed profile management later

--- app/routes.ts
+++ app/routes.ts
@@ -12,6 +12,7 @@ export const appRoutes:Routes = [
   { path: 'events', component: EventsListComponent, resolve : { events: EventListResolver}},
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
   { path: '404', component: Error404Component },
-  { path: '', redirectTo: '/events', pathMatch: 'full'}
+  { path: '', redirectTo: '/events', pathMatch: 'full'},
+  { path: 'user', loadChildren : 'app/user/user.module#UserModule'}
 
 ]
