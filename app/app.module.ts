dc63dd2c9f79b5898900e73632e0d46180477796
scott james <scottejames@gmail.com>
2017-05-02 13:44:25 +0100

    Listing session information on an event

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,7 +11,9 @@ import {
 	CreateEventComponent,
 	EventRouteActivator,
 	EventListResolver,
-    CreateSessionComponent
+    CreateSessionComponent,
+	SessionListComponent
+
 } from './events/index'
 
 import {EventsAppComponent} from './events-app.component'
@@ -35,7 +37,8 @@ import {AuthService} from './user/auth.service'
 		EventDetailsComponent,
 		CreateEventComponent,
 		Error404Component,
-        CreateSessionComponent
+        CreateSessionComponent,
+		SessionListComponent
 	],
 	providers: [
 		EventService,
