52523151619411803d9491685c0e78a85ee2a387
scott james <scottejames@gmail.com>
2017-05-03 17:06:25 +0100

    enabled filtering of session data

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnChanges } from '@angular/core'
 import { ISession } from '../shared/index'
 
 @Component ({
@@ -7,6 +7,27 @@ import { ISession } from '../shared/index'
 
 })
 
-export class SessionListComponent{
+export class SessionListComponent implements OnChanges{
     @Input() sessions: ISession[]
+    @Input() filterBy: string
+
+    visableSessions : ISession[] = [];
+
+    ngOnChanges(){
+        if(this.sessions){
+            this.filterSessions(this.filterBy)
+        }
+    }
+
+    filterSessions(filter: string) {
+        if (filter === 'all') {
+            this.visableSessions = this.sessions.slice(0);
+        } else {
+            this.visableSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter;
+            })
+
+        }
+    }
+
 }
