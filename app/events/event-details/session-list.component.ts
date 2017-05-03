726c137b5d1156ea10d1a860a6df6ebe9881d467
scott james <scottejames@gmail.com>
2017-05-03 17:25:12 +0100

    SOrting Sessions

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -10,12 +10,14 @@ import { ISession } from '../shared/index'
 export class SessionListComponent implements OnChanges{
     @Input() sessions: ISession[]
     @Input() filterBy: string
+    @Input() sortBy: string
 
     visableSessions : ISession[] = [];
 
     ngOnChanges(){
         if(this.sessions){
-            this.filterSessions(this.filterBy)
+            this.filterSessions(this.filterBy);
+            this.sortBy === 'name' ? this.visableSessions.sort(sortByNameAsc) : this.visableSessions.sort(sortByVotesDec);
         }
     }
 
@@ -30,4 +32,14 @@ export class SessionListComponent implements OnChanges{
         }
     }
 
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession){
+    if (s1.name > s2.name) return 1;
+    else if (s1.name === s2.name) return 0;
+    else return -1;
+}
+
+function sortByVotesDec(s1: ISession, s2: ISession){
+    return s2.voters.length - s1.voters.length;
 }
